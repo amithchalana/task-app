850fe6c108d47a95ebabfe35ddf01f6ca7700f1f
chalana <amithchalana19@gmail.com>
2023-01-21 19:29:27 +0530

    Add hibernate validator

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -4,16 +4,15 @@ import lk.ijse.dep9.app.dto.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/users")
-@CrossOrigin
-
 public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("application/json")
+    @PostMapping(consumes = "application/json")
     public void createUserAccount(@RequestBody UserDTO user) {
-
+        System.out.println(user);
     }
 
     @PatchMapping("/me")

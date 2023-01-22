df079e0847c0a255e7fb2172dd029b18efc629f5
chalana <amithchalana19@gmail.com>
2023-01-22 07:17:15 +0530

    Handle validation exception smoothly

--- src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -1,15 +1,38 @@
 package lk.ijse.dep9.app.advice;
 
-import lk.ijse.dep9.app.dto.ErrorResponseMessageDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Throwable.class)
-    public ErrorResponseMessageDTO uncaughtExceptions(Throwable t) {
-        return new ErrorResponseMessageDTO(t.getMessage(),405);
+//    @ExceptionHandler(Throwable.class)
+//    public ErrorResponseMessageDTO uncaughtExceptions(Throwable t) {
+//        t.printStackTrace();
+//        return new ErrorResponseMessageDTO(t.getMessage(),405);
+//
+//    }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,Object> validationException(MethodArgumentNotValidException e) {
+//        String message = e.getFieldErrors().stream().map(err -> err.getField() + ":" + err.getDefaultMessage() + ",")
+//                .reduce((prev, curr) -> prev + curr).get();
+//        return new ErrorResponseMessageDTO(message,400);
+
+        HashMap<String, Object> errAttributes = new LinkedHashMap<>();
+        errAttributes.put("status",HttpStatus.BAD_REQUEST.value());
+        errAttributes.put("status",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errAttributes.put("status",new Date().toString());
+        List<String> validationList = e.getFieldErrors().stream().map(err -> err.getField() + ": " +
+                err.getDefaultMessage()).collect(Collectors.toList());
+        errAttributes.put("errors",validationList);
+        return errAttributes;
     }
 }
--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -2,9 +2,13 @@ package lk.ijse.dep9.app.api;
 
 import lk.ijse.dep9.app.dto.UserDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -13,8 +17,12 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public void createUserAccount(@Valid  @RequestBody UserDTO user) {
+    public void createUserAccount(@Valid  @RequestBody UserDTO user/*, Errors errors*/) {
         System.out.println(user);
+//        Optional<FieldError> fieldError = errors.getFieldErrors().stream().findFirst();
+//        if (fieldError.isPresent()) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,fieldError.get().getDefaultMessage());
+//        }
     }
 
     @PatchMapping("/me")

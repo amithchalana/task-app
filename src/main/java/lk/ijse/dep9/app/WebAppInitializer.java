682fee8e8ca50dac1615f83a60a3c6509ee0f0a5
chalana <amithchalana19@gmail.com>
2023-01-22 18:26:22 +0530

    Add Apache common codec

--- src/main/java/lk/ijse/dep9/app/WebAppInitializer.java
+++ src/main/java/lk/ijse/dep9/app/WebAppInitializer.java
@@ -1,7 +1,10 @@
 package lk.ijse.dep9.app;
 
+import lk.ijse.dep9.app.api.filter.SecurityFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 
 @Slf4j
@@ -24,5 +27,11 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new SecurityFilter()};
     }
 }

874fad49f23a9e18124008cf8ea65a37eecd9cba
chalana <amithchalana19@gmail.com>
2023-10-14 11:33:35 +0530

    initial commit

---
+++ src/main/java/com/chalana/taskappbackend/TaskAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.chalana.taskappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chalana/taskappbackend/TaskAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalana.taskappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

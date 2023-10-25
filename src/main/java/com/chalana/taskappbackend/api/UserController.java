cc5c0d65da86c6d87b2c8f63b0eab6cf26738ac1
chalana <amithchalana19@gmail.com>
2023-10-25 09:27:08 +0530

    Add the service facade

--- src/main/java/com/chalana/taskappbackend/api/UserController.java
+++ src/main/java/com/chalana/taskappbackend/api/UserController.java
@@ -3,10 +3,12 @@ package com.chalana.taskappbackend.api;
 import com.chalana.taskappbackend.dto.UserDTO;
 import com.chalana.taskappbackend.entity.User;
 import com.chalana.taskappbackend.service.custom.UserService;
+import com.chalana.taskappbackend.util.ValidationGroups;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,18 +29,15 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public void createUserAccount(@Valid @RequestBody UserDTO user) {
+    public void createUserAccount(@Validated(ValidationGroups.Create.class) @RequestBody UserDTO user) {
 
-//        userService.createNewUserAccount(user);
-        System.out.println("successfully invoke");
-        System.out.println(user);
+        System.out.println("invoke create user");
 
     }
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping( value = "/me",consumes = "application/json")
-    public void updateUserAccountDetails(@Valid @RequestBody UserDTO user ) {
-        System.out.println("Invoke updateUserAccountDetails");
-        System.out.println(user);
+    public void updateUserAccountDetails(@Validated(ValidationGroups.Update.class) @RequestBody UserDTO user ) {
+        System.out.println("invoke create user");
     }
 
 
@@ -47,12 +46,13 @@ public class UserController {
     @GetMapping(value = "/me",produces = "application/json")
     public UserDTO getUserAccountDetails( @AuthenticationPrincipal(expression = "username") @RequestBody String username){
 //        return userService.getUserAccountDetails(username);
+        System.out.println("invoke get user account");
         return null;
     }
 
 
     @DeleteMapping("/me")
     public void deleteUserAccount() {
-
+        System.out.println("invoke delete user");
     }
 }
---
+++ src/main/java/com/chalana/taskappbackend/service/custom/ProjectTaskService.java
@@ -0,0 +1,6 @@
+package com.chalana.taskappbackend.service.custom;
+
+import com.chalana.taskappbackend.service.SuperService;
+
+public interface ProjectTaskService extends SuperService {
+}
